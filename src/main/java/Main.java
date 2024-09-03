6d403e1b32f05ea03263e9b82d93ba8bcaeefd49
Eduardo Santos <edumasa@gmail.com>
2024-09-03 20:59:20 -0300

    Refactore usando lombok!

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import diome.banco.cliente.Cliente;
+import diome.banco.conta.Conta;
+import diome.banco.conta.ContaCorrente;
+import diome.banco.conta.ContaPoupanca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
